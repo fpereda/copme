b0ce7f55c44691f60248a7e0c075cd6852a2014f
Fernando J. Pereda <ferdy@ferdyx.org>
2007-08-14 11:05:53 +0200

    Add some sort of doxygen docs.
    Signed-off-by: Fernando J. Pereda <ferdy@ferdyx.org>

--- copme/copme.h
+++ copme/copme.h
@@ -29,6 +29,11 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+/**
+ * @file copme.h
+ * @brief Main header file for copme.
+ */
+
 #ifndef COPME_GUARD_COPME_H
 #define COPME_GUARD_COPME_H 1
 
@@ -44,73 +49,156 @@
 
 #define COPME_NOPTV_GROWTH_RATE (2.5)
 
+/**
+ * Holds information about an option argument.
+ */
 struct copme_arg {
-	char *data;
-	unsigned specified : 1;
+	char *data;              /**< Data supplied by the user */
+	unsigned specified : 1;  /**< Whether the argument was specified */
 };
 
+/**
+ * Define whether an option has a required argument or not.
+ */
+enum copme_arg_kind {
+	COPME_NOARG,   /**< No argument for this option */
+	COPME_HASARG,  /**< Required argument for this option */
+	COPME_OPTARG   /**< Optional argument for this option */
+};
+
+/**
+ * Defines a long option.
+ */
 struct copme_long {
-	char *lname;
-	char sname;
-	char *desc;
-	enum copme_arg_kind {
-		COPME_NOARG,
-		COPME_HASARG,
-		COPME_OPTARG
-	} arg_kind;
-	struct copme_arg *arg;
-	unsigned specified;
+	char *lname;                   /**< Long name */
+	char sname;                    /**< Short name */
+	char *desc;                    /**< Description of the option */
+	enum copme_arg_kind arg_kind;  /**< Define the kind of argument */
+	struct copme_arg *arg;         /**< Pointer to our copme_arg */
+	unsigned specified;            /**< Times this option was specified */
 };
 
+/**
+ * Group of options.
+ */
 struct copme_group {
-	char *sdesc;
-	char *ldesc;
-	struct copme_long *opts;
+	char *sdesc;              /**< Short description or name of the group */
+	char *ldesc;              /**< Long description of the group */
+	struct copme_long *opts;  /**< Array of options belonging to this group */
 };
 
+/**
+ * Arguments that weren't options.
+ */
 struct copme_nopts {
-	unsigned count;
-	char **noptv;
-	size_t size;
+	unsigned count;  /**< Number of no options */
+	char **noptv;    /**< Array of pointers to those no options */
+	size_t size;     /**< Number of elements there is space allocated for */
 };
 
+/**
+ * State of a copme parsing.
+ */
 struct copme_state {
-	int argc;
-	char **argv;
-	unsigned argind;
-	struct copme_nopts nopts;
-	struct copme_group *groups;
-	struct copme_long *curopt;
-	unsigned finished : 1;
-	unsigned error    : 1;
+	int argc;                    /**< Number of arguments */
+	char **argv;                 /**< Array of arguments */
+	unsigned argind;             /**< Current argument we are parsing */
+	struct copme_nopts nopts;    /**< Store no options as we find them */
+	struct copme_group *groups;  /**< Groups of options to recognize */
+	struct copme_long *curopt;   /**< Current long option */
+	unsigned finished : 1;       /**< Whether we are finished or not */
+	unsigned error    : 1;       /**< Whether we are in error state */
 };
 
+/**
+ * Gets a pointer to a copme_long in various groups given its name.
+ *
+ * @param groups Groups where the option is.
+ * @param lname Long name identifying the option to find.
+ * @return Pointer to said option or NULL if not found.
+ */
 struct copme_long *
 copme_option_named(struct copme_group *groups, char *lname);
 
+/**
+ * Allocate and init a copme_state. A copme_state holds information about the
+ * state of the library. This pointer should be freed with copme_free once the
+ * caller is done with it.
+ *
+ * @see copme_free
+ * @param groups Groups defining the options to extract
+ * @param argc Number of arguments
+ * @param argv Array of arguments
+ * @return Pointer to a copme_state holding a new parser.
+ */
 struct copme_state *
 copme_init(struct copme_group groups[], int argc, char *argv[]);
 
+/**
+ * Completely free a copme_state. Once the client has finished parsing options
+ * and extracting info, it should free the resources allocated for the parser
+ * using this function.
+ *
+ * @param st Pointer to the parser to free.
+ */
 void copme_free(struct copme_state *st);
+
+/**
+ * Parse next option.
+ *
+ * @param st Pointer to the parser.
+ */
 void copme_next(struct copme_state *st);
+
+/**
+ * Display usage information for a parser.
+ *
+ * @param st Parser to show information for.
+ * @param pre Function to call before displaying usage information.
+ * @param post Function to call after everything has been displayed.
+ */
 void copme_usage(struct copme_state *st,
 		void (*pre)(void), void (*post)(void));
 
+/**
+ * Whether a parser has finished or not.
+ *
+ * @param st Parser.
+ * @return 0 if it has not finished. != 0 otherwise.
+ */
 static inline unsigned copme_finished(struct copme_state *st)
 {
 	return st->finished || st->argind >= st->argc;
 }
 
+/**
+ * Whether the parser is in error state.
+ *
+ * @param st Parser.
+ * @return 0 if it is not in error state. != 0 otherwise.
+ */
 static inline unsigned copme_error(struct copme_state *st)
 {
 	return st->error;
 }
 
+/**
+ * Get a pointer to the current option parser.
+ *
+ * @param st Parser
+ * @return Pointer to the current option.
+ */
 static inline struct copme_long *copme_current_opt(struct copme_state *st)
 {
 	return st->curopt;
 }
 
+/**
+ * Get no options for a parser.
+ *
+ * @param st Parser
+ * @return Pointer to a copme_nopts with information about no options found.
+ */
 static inline struct copme_nopts *copme_nopts(struct copme_state *st)
 {
 	return &st->nopts;
